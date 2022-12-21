87aef7cd0c6d70668e374c681017a097933a8329
takesha17 <isaevtayar209@gmail.com>
2022-12-22 00:02:57 +0600

    updated admin page

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,11 +3,12 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .models import Choice, Question
 from django.template import loader
 from django.urls import reverse
+from django.views import generic
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
+    template = loader.get_template('polls/index1.html')
     context = {
         'latest_question_list': latest_question_list,
     }
@@ -35,3 +36,22 @@ def vote(request, question_id):
         selected_choice.save()
         
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+    
+class IndexView(generic.ListView):
+    template_name = 'polls/index1.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
